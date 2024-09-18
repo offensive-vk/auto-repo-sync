8b1242cc89482bde71a2ff16c690f56f0719a327
Vedansh <superuser.ntsystems@outlook.com>
2024-09-18 13:19:27 +0530

    Tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 ## Licensed Under MIT. 2024 - Present ###
 #########################################
 
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL}
+git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL//https:\//}
 
 case "${GITHUB_EVENT_NAME}" in
     push)
