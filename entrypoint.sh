c0c803693bdc480ac3920b4dbab8a7d640743cc7
Vedansh <superuser.ntsystems@outlook.com>
2024-12-11 13:52:09 +0000

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,25 +20,21 @@ else
     git remote add target "$TARGET_URL"
 fi
 
-# Handle events
+# Fetch latest changes from the base repository
+git fetch origin --prune
+
+# Push all references to the target repository, skipping deletion actions
 case "${GITHUB_EVENT_NAME}" in
     push)
         echo "Handling push event..."
-        git push --prune --mirror target
-        ;;
-    delete)
-        echo "Handling delete event..."
-        if [ -z "$GITHUB_EVENT_REF" ]; then
-            echo "Error: GITHUB_EVENT_REF is not set for delete event."
-            exit 1
-        fi
-        git push -d target "$GITHUB_EVENT_REF"
+        git push target --all
+        git push target --tags
         ;;
     *)
         echo "Handling other event: ${GITHUB_EVENT_NAME}."
-        git status
-        git push target "$GITHUB_EVENT_REF"
+        git push target --all
+        git push target --tags
         ;;
 esac
 
-echo ":: Repository sync completed successfully. ::"
+echo ":: Repository sync completed successfully. ::
